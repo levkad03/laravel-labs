397713b6740cbee8a58e76ffc50a9f8770e25c29
Lev Kadatskyi <levkadatscky@gmail.com>
2025-05-24 10:47:48 +0300

    added controllers, routed them in routes

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,15 @@ use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
+use APP\Http\Controllers\CommentController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
 Route::apiResource('projects', ProjectController::class);
+Route::apiResource('tasks', TaskController::class);
+Route::apiResource('users', UserController::class);
+Route::apiResource('comments', CommentController::class);
